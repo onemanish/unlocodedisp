a7062dacb27878224e928bec2fa084768075ee2a
Manish Srivastava <onemanish@gmx.com>
2024-04-28 01:26:30 +0900

    Changed method of conversion of deg/min to decimals. Refactored some long variable names.

--- unlo-disp.py
+++ unlo-disp.py
@@ -6,21 +6,27 @@ import streamlit as st
 from streamlit_folium import st_folium
 
 @st.cache_data
-def get_unlocodes():
-    df = pd.read_excel('2023-2UNLOCODECodeList.xlsx')
+def get_unlocodes(): # split the coords col to lat long and convert to decimal
+    df = pd.read_excel('2023-2UNLOCODECodeList.xlsx') # read the date file with UNLO Codes
     print('read the data')
-    df.dropna(subset=['Coordinates'], inplace=True)
-    df['slat'] = df['Coordinates'].str.split().str[0]
-    df['slong'] = df['Coordinates'].str.split().str[1]
-    df['lat'] = df['slat'].str.extract('(\d+)').astype(int)/100
-    df.loc[df['slat'].str.endswith('S'), 'lat'] *= -1
-    df['long'] = df['slong'].str.extract('(\d+)').astype(int)/100
-    df.loc[df['slong'].str.endswith('W'), 'long'] *= -1
+    
+    def dddmmX_decimal(degVal):
+        direction = degVal[-1]  # Extract the direction ('N' or 'S')
+        degVal = degVal[:-1]  # Remove the direction
+        degrees = int(degVal[:-2])  # Extract the degrees part
+        minutes = int(degVal[-2:])  # Extract the minutes part
+        decimal_degrees = degrees + (minutes / 60)
+        if direction == 'S' or direction == 'W':
+            decimal_degrees *= -1  # If direction is S or W, make decimal degrees negative
+        return decimal_degrees
+
+    df.dropna(subset=['Coordinates'], inplace=True) # throw out rows without lat/long info
+    df['lat'] = df['Coordinates'].str.split().str[0].apply(dddmmX_decimal)
+    df['long'] = df['Coordinates'].str.split().str[1].apply(dddmmX_decimal)
     return df
 
 st.set_page_config(layout='wide', page_title='UN/LOCODES')
 st.sidebar.title('UN/LOCODE locator')
-# vName = st.sidebar.text_input('Enter Vessel Name', value='I am here!')
 vLat = st.sidebar.number_input('Latitude (use -ve value for S)', min_value=-90, max_value=90, value=8)
 vLong = st.sidebar.number_input('Longitude (use -ve value for W)', min_value=-180, max_value=180, value=77)
 diff = st.sidebar.number_input('Show UN/LO Codes around (º)', value=2)
@@ -29,32 +35,23 @@ mapZoom = st.sidebar.number_input('Map zoom', value=9)
 st.subheader('UN/LOCode Viewer  -- click on marker to select')
 
 df1 = get_unlocodes()
-filtered_df = df1[(df1['lat'] >= vLat - diff) & (df1['lat'] <= vLat + diff)] # remove all points diffº far from my location
-filtered_df = filtered_df[(filtered_df['long'] >= vLong - diff) & (filtered_df['long'] <= vLong + diff)] # remove all points diffº far from my location
-filtered_df.reset_index(drop=True, inplace=True) # to be able to address each point separately
+sel_df = df1[(df1['lat'] >= vLat - diff) & (df1['lat'] <= vLat + diff)] # remove all points diffº far from my location
+sel_df = sel_df[(sel_df['long'] >= vLong - diff) & (sel_df['long'] <= vLong + diff)] # remove all points diffº far from my location
+sel_df.reset_index(drop=True, inplace=True) # to be able to address each point sequentially
 
-m = folium.Map(location=[vLat, vLong], tiles="OpenStreetMap", zoom_start=mapZoom) # set up map
-folium.Marker(
-      location=[vLat, vLong],
-      tooltip=folium.Tooltip('I am here!'),
-      icon=folium.Icon(color='orange')
-   ).add_to(m)
+ # set up map and add markers
+m = folium.Map(location=[vLat, vLong], tiles="OpenStreetMap", zoom_start=mapZoom)
 
-folium.Circle(
-    location=[vLat, vLong], radius=vCircle*1852,
-    color="black", weight=1,
-    opacity=1, fill_opacity=0.2,
-    fill_color="green", fill=False,  
-    tooltip=f"{vCircle}NM",
-).add_to(m)
+folium.Marker(location=[vLat, vLong], tooltip=folium.Tooltip('I am here!'), icon=folium.Icon(color='orange')).add_to(m)
+folium.Circle(location=[vLat, vLong], radius=vCircle*1852, color="black", weight=1, \
+    opacity=1, fill_opacity=0.2, fill_color="green", fill=False, tooltip=f"{vCircle}NM").add_to(m)
 
 print('-----done-----')
 
-for i in range(0,len(filtered_df)):
-    dist = haversine((vLat,vLong), (filtered_df.iloc[i]['lat'], filtered_df.iloc[i]['long']), unit=Unit.NAUTICAL_MILES)
-    folium.Marker(
-      location=[filtered_df.iloc[i]['lat'], filtered_df.iloc[i]['long']],
-      tooltip=f"{filtered_df.iloc[i]['Name']}-{filtered_df.iloc[i]['Country']}{filtered_df.iloc[i]['Location']}-{dist:.1f}NM away",
+for i in range(0,len(sel_df)):
+    dist = haversine((vLat,vLong), (sel_df.iloc[i]['lat'], sel_df.iloc[i]['long']), unit=Unit.NAUTICAL_MILES)
+    folium.Marker(location=[sel_df.iloc[i]['lat'], sel_df.iloc[i]['long']],
+      tooltip=f"{sel_df.iloc[i]['Name']}-{sel_df.iloc[i]['Country']}{sel_df.iloc[i]['Location']}-{dist:.1f}NM away",
    ).add_to(m)
 
 st_data = st_folium(m, use_container_width=True)
