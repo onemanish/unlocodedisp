69cd447e3204795b9a9dec821eeee0419215f4b1
Manish Srivastava <onemanish@gmx.com>
2024-09-09 14:36:24 +0900

    Added ability to click on the map to set new location.

--- unlo-disp.py
+++ unlo-disp.py
@@ -32,12 +32,22 @@ def get_unlocodes(): # split the coords col to Lat Long and convert to decimal
     df['Long'] = df['Coordinates'].str.split().str[1].apply(deg2dec)
     return df
 
+def get_dist(row):
+    return haversine((row['Lat'], row['Long']), (vLat, vLong), unit=Unit.NAUTICAL_MILES)
+    
+
+if 'vLat' not in st.session_state:
+    st.session_state.vLat = 19.0
+if 'vLong' not in st.session_state:
+    st.session_state.vLong = 72.5
+
+# Update the sidebar inputs to use session state
 # print('------ start --------')
 # Set page parameters
 st.set_page_config(layout='wide', page_title='UN/LOCODES')
-st.sidebar.subheader('Own location')
-vLat = st.sidebar.number_input('My Latitude (use -ve decimal for S)', min_value=-90.0, max_value=90.0, value=19.0)
-vLong = st.sidebar.number_input('My Longitude (use -ve decimal for W)', min_value=-180.0, max_value=180.0, value=72.5)
+st.sidebar.subheader('Click on map to set own location')
+vLat = st.sidebar.number_input('My Latitude (use -ve decimal for S)', min_value=-90.0, max_value=90.0, value=st.session_state.vLat)
+vLong = st.sidebar.number_input('My Longitude (use -ve decimal for W)', min_value=-180.0, max_value=180.0, value=st.session_state.vLong)
 vCircle = st.sidebar.number_input('Draw Circle Around Me (NM)', value=20)
 st.sidebar.divider()
 diff = st.sidebar.number_input('Show UN/LO Codes around (º)', value=1.0)
@@ -49,15 +59,12 @@ df1 = get_unlocodes()
 sel_df = df1[(df1['Lat'] >= vLat - diff) & (df1['Lat'] <= vLat + diff)] # remove all points diff deg far from my location
 sel_df = sel_df[(sel_df['Long'] >= vLong - diff) & (sel_df['Long'] <= vLong + diff)] # remove all points diff º far from my location
 
-def get_dist(row):
-    return haversine((row['Lat'], row['Long']), (vLat, vLong), unit=Unit.NAUTICAL_MILES)
-    
 sel_df['Distance'] = sel_df.apply(get_dist, axis=1) # Add column for distances 
 sel_df = sel_df.sort_values(by='Distance')
 sel_df.reset_index(drop=True, inplace=True) # to be able to address each point sequentially
-st.error(f'{len(sel_df)} UN/LO Code locations found within {diff}º from my position ({vLat}º, {vLong}º).')
+st.error(f'{len(sel_df)} UN/LO Code locations found within {diff}º from my position ({vLat}º, {vLong}º).') # no error, just color change
 # st.write(sel_df)
-styled_df = sel_df.style.apply(lambda x: ["background-color: darkred" if val == "Y" else "" for val in x], axis=1)
+styled_df = sel_df.style.apply(lambda x: ["background-color: pink" if val == "Y" else "" for val in x], axis=1)
 styled_df = styled_df.format("{:.2f}", subset=pd.IndexSlice[:, ["Lat", 'Long','Distance']])
 st.dataframe(styled_df)
 
@@ -76,6 +83,12 @@ for i in range(0,len(sel_df)):
 
 st_data = st_folium(m, use_container_width=True)
 selData = st_data['last_object_clicked_tooltip']
+# Update coordinates when map is clicked
+if st_data['last_clicked']:
+    st.session_state.vLat = st_data['last_clicked']['lat']
+    st.session_state.vLong = st_data['last_clicked']['lng']
+    st.experimental_rerun()
+
 # st.warning(selData)
 if selData != None:
     selData = "  --  ".join(st_data['last_object_clicked_tooltip'].split('-'))
